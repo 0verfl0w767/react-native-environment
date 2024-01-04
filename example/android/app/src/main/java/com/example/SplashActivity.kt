2292ebd22ac36e6a4658e33d0d6e603b23548a59
0verfl0w767 <tkddbs7631@naver.com>
2024-01-05 03:12:56 +0900

    fix: changed deprecated code

--- example/android/app/src/main/java/com/example/SplashActivity.kt
+++ example/android/app/src/main/java/com/example/SplashActivity.kt
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+import android.os.Looper
+
 class SplashActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
 
-    Handler().postDelayed({
+    Handler(Looper.getMainLooper()).postDelayed({
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
         startActivity(intent)
@@ -20,10 +22,10 @@ class SplashActivity : AppCompatActivity() {
 
   }
   companion object {
-    private const val DURATION : Long = 3000
+    private const val DURATION : Long = 4000
   }
 
   override fun onBackPressed() {
-    super.onBackPressed()
+    onBackPressedDispatcher.onBackPressed()
   }
 }
